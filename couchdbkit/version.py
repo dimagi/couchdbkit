952ea52fdb940c42834100f66f1e402109dcd7c3
Ben Rudolph <rudolphben@gmail.com>
2017-05-22 15:26:19 +0200

    proper sem ver version

--- couchdbkit/version.py
+++ couchdbkit/version.py
@@ -3,5 +3,5 @@
 # This file is part of couchdbkit released under the MIT license.
 # See the NOTICE for more information.
 
-version_info = (0, 8, 0, 3)
+version_info = (0, 8, 1, 0)
 __version__ =  ".".join(map(str, version_info))
