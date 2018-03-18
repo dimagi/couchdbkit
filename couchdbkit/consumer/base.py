046325fcdeffd4308247c4a88c0dd31c99465547
Jonathan Emord <jonathan.emord@gmail.com>
2018-03-18 16:13:08 +0530

    Change feeds stop referencing restkit

--- couchdbkit/consumer/base.py
+++ couchdbkit/consumer/base.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -
 #
-# This file is part of couchdbkit released under the MIT license. 
+# This file is part of couchdbkit released under the MIT license.
 # See the NOTICE for more information.
+
 
 def check_callable(cb):
     if not callable(cb):
         raise TypeError("callback isn't a callable")
+
 
 class ConsumerBase(object):
 
@@ -13,19 +15,30 @@ class ConsumerBase(object):
         self.db = db
 
     def fetch(self, cb=None, **params):
-        resp = self.db.res.get("_changes", **params)
-        if cb is not None:
-            check_callable(cb)
-            cb(resp.json_body)
+        changes = self.db.cloudant_database.changes(**params)
+        try:
+            change = next(changes)
+        except StopIteration:
+            return {
+                'last_seq': changes.last_seq,
+                'results': []
+            }
         else:
-            return resp.json_body
+            if cb is not None:
+                check_callable(cb)
+                cb(change)
+            else:
+                return {
+                    'last_seq': change['seq'],
+                    'results': [change]
+                }
 
     def wait_once(self, cb=None, **params):
         raise NotImplementedError
 
     def wait(self, cb, **params):
         raise NotImplementedError
-    
+
     def wait_once_async(self, cb, **params):
         raise NotImplementedError
 
