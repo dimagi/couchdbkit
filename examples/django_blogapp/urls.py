17a6b817f6895489e296303e10b265ea870e1cd7
Nick Pellegrino <npellegrino@dimagi.com>
2018-07-25 17:05:18 -0400

    kill examples

--- examples/django_blogapp/urls.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from django.conf.urls import patterns, url
-
-
-urlpatterns = patterns('',
-    url(r'^$', 'django_blogapp.blog_app.views.home'),
-    url(r'^post/(?P<post_id>\w*)/$', 'django_blogapp.blog_app.views.view_post'),
-    url(r'^post/edit/(?P<post_id>\w*)/$', 'django_blogapp.blog_app.views.edit_post'),
-)
