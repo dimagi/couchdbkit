c697a2d1bb0018d5db5c648b28cbc2b89d5ffbac
Nick Pellegrino <npellegrino@dimagi.com>
2018-11-15 20:36:45 -0500

    use sorted function, again

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1294,14 +1294,14 @@ class PropertyTestCase(unittest.TestCase):
             'slm': [{'doc_type': 'A', 's': six.text_type(a1.s)},
                     {'doc_type': 'A', 's': six.text_type(a2.s)}]
         })
-        b.slm.sort(key=lambda item: item['s'], reverse=True)
+        b.slm = sorted(b.slm, key=lambda item: item['s'], reverse=True)
         self.assertEqual([b.slm[0].s, b.slm[1].s], [a2.s, a1.s])
         self.assertEqual(b._doc, {
             'doc_type': 'B',
             'slm': [{'doc_type': 'A', 's': six.text_type(a2.s)},
                     {'doc_type': 'A', 's': six.text_type(a1.s)}]
         })
-        b.slm.sort(cmp=lambda x, y: cmp(x['s'].lower(), y['s'].lower()))
+        b.slm = sorted(b.slm, cmp=lambda x, y: cmp(x['s'].lower(), y['s'].lower()))
         self.assertEqual([b.slm[0].s, b.slm[1].s], [a1.s, a2.s])
         self.assertEqual(b._doc, {
             'doc_type': 'B',
