c200e32918c60d9d8408768d25bbf4c9ead35938
Jonathan Emord <emord@users.noreply.github.com>
2019-02-12 20:40:18 -0500

    Remove version dependency on six

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires = [
         'jsonobject>=0.9.8',
         'cloudant~=2.7',
-        'six==1.11.0',
+        'six',
     ],
     provides=['couchdbkit'],
     obsoletes=['couchdbkit'],
